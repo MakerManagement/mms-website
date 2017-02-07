02604b7d35fca41a1ca401e21db04bb7d920f067
Thomas Bergby <thomas.bergby@gmail.com>
2017-02-07 15:37:26 +0100

    Added folder structure - Added main css file - Added Makerspace logo - Added wrapper and index files, index.php includes wrapper.html

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Main</title>
+    <link rel="stylesheet" type="text/css" href="sources/css/main.css"/>
 </head>
 <body>
+<?php
+    include("sources/html/wrapper.html");
+?>
     <h1>Hello world!</h1>
 </body>
 </html>
