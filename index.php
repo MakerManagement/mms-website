f79a4104caff0598f50751d3e1ff5a4ba10cb13a
Thomas Bergby <thomas.bergby@gmail.com>
2017-02-11 00:12:18 +0100

    Added majord CSS changes. - Search field is now super fancy - Makerspace logo fits - Layout is now finished. TODO: - Get content from JSON file - Show the content in a structured way

--- index.php
+++ index.php
@@ -2,13 +2,27 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Main</title>
+    <title>Makerspace Management System</title>
     <link rel="stylesheet" type="text/css" href="sources/css/main.css"/>
+    <!-- Importing font for search icon !-->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
 <?php
     include("sources/html/wrapper.html");
 ?>
-    <h1>Hello world!</h1>
+<h1>Lorem Ipsum</h1>
+<p>
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean velit arcu, molestie in porttitor sed, egestas ut lorem. Aliquam erat volutpat. Duis convallis nulla lobortis, luctus lacus vitae, aliquet nibh. Quisque dapibus cursus vehicula. Etiam lacinia sollicitudin mi, vel tempor nisl commodo sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec congue eros non magna dictum, in posuere lacus aliquet. Morbi sollicitudin dui sed sagittis mattis. In cursus volutpat nibh quis pharetra. Pellentesque lacus dui, porta nec interdum at, ultrices aliquam turpis. Mauris ut sapien vitae erat tempor imperdiet eu in arcu. Nulla nec dolor massa. Proin sollicitudin ac mauris eget faucibus.
+</p>
+    Donec commodo, tellus non facilisis dapibus, purus ex vestibulum sem, non rhoncus mi neque finibus purus. Nulla semper tellus quis cursus congue. Sed vehicula, velit et luctus facilisis, nibh justo varius risus, at vulputate turpis dolor non tortor. Proin malesuada diam et ornare eleifend. Aliquam vitae sapien sit amet arcu dignissim tincidunt. Nulla a fermentum augue. Sed posuere, nisi eu tristique rhoncus, sapien nunc dapibus lacus, id lobortis metus est vel velit.
+<p>
+    Quisque rhoncus tincidunt sapien ut porttitor. Morbi vulputate dapibus arcu eu elementum. Etiam ac nunc nulla. Aenean venenatis leo turpis, sed varius ligula hendrerit vel. Donec volutpat nulla ac nisl fermentum sodales. Curabitur quis elementum ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut in tortor tincidunt, aliquet metus nec, cursus eros. Sed interdum viverra convallis. Aenean dapibus purus at dapibus scelerisque. Donec finibus dui vel odio convallis mollis nec a erat. Aliquam a nisi sem. Curabitur accumsan ex urna, at pharetra eros gravida eget. Mauris diam erat, pharetra a felis vel, euismod tempor sem.
+</p>
+    Nulla in libero vitae ligula ultricies finibus nec at nunc. Donec tristique arcu felis, in vehicula diam dapibus at. Donec dictum tristique feugiat. Vivamus pretium odio sagittis, vehicula urna auctor, tristique nisi. Morbi vestibulum id metus in auctor. Mauris scelerisque aliquet mollis. Suspendisse potenti. Morbi tempor, justo iaculis dictum finibus, sem elit semper lorem, sed euismod orci arcu eget turpis. Sed ut ultricies massa.
+<p>
+    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus non nisi metus. Donec interdum nisi sed ultricies volutpat. Donec facilisis pellentesque nulla eu lacinia. Duis tempor orci et lacinia laoreet. Nulla facilisi. Praesent consectetur, metus vitae accumsan efficitur, odio erat auctor nibh, non pellentesque ligula dolor non dui. Donec malesuada id lacus eget tempus.
+</p>
 </body>
 </html>
