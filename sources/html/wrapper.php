55394f7f07773af11ea2e68ecd9a568fc0e05169
Thomas Bergby <thomas.bergby@gmail.com>
2017-02-14 15:37:56 +0100

    Added multi language support
    Still need a frontend option to change language

---
+++ sources/html/wrapper.php
@@ -0,0 +1,22 @@
+<div id="top-banner">
+    <div id="wrapper-logo">
+        <img class="resize_fit_center" src="sources/logos/logo-lang.png" alt="MakerSpace logo" />
+    </div>
+
+    <div class="search-box">
+        <div class="search-wrapper">
+            <span class="icon"><i class="fa fa-search"></i></span>
+            <input type="search" id="search" placeholder="Search for item">
+        </div>
+    </div>
+</div>
+
+<div id="aside-box">
+    <aside>
+        <h2><?php
+            //echo $lang;
+            echo $lang["CATEGORY"]; ?></h2>
+        <ul id="category-list">
+        </ul>
+    </aside>
+</div>
---
+++ sources/lang/lang.no.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: thobber
+ * Date: 14.02.17
+ * Time: 13:35
+ */
+/*
+------------------
+Language: Norwegian
+------------------
+*/
+
+$lang = array();
+$lang["PAGE_TITLE"] = "MakerSpace Management System";
+$lang["MAIN_HEADER"] = "MakerSpace Inventar";
+$lang["CATEGORY"] = "Kategori";
