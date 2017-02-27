2e1cc19b471b3601c5e9c7f478a3adbddeed1c1d
Thomas Bergby <thomas.bergby@gmail.com>
2017-02-27 18:13:24 +0100

    Cleaned up some structure in the code

--- sources/js/script.js
+++ sources/js/script.js
@@ -151,13 +151,16 @@ function setParam(name, value)
 }
 
 // Takes a string, and if the string is over 75 characters, it will add "..." to it
-function truncate(string){
+function truncate(string)
+{
     if (string.length > maxcharacters)
-        { //noinspection JSUnresolvedFunction
+    { //noinspection JSUnresolvedFunction
             return string.substring(0,maxcharacters) + '...';
-        }
+    }
     else
+    {
         return string;
+    }
 }
 
 
