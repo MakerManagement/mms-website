3220fefb5b021fb22e8fb66a394c091ed42c6d36
Thomas Bergby <thomas.bergby@gmail.com>
2017-03-31 14:00:39 +0200

    Made js file async

--- sources/html/wrapper.php
+++ sources/html/wrapper.php
@@ -4,7 +4,7 @@
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <script src="sources/js/script.js"></script>
+    <script src="sources/js/script.js" async></script>
 </head>
 
 <div id="language-chooser">
